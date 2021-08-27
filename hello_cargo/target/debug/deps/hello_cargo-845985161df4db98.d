6cc1ad0f9f57aea0c4d0205bb48499fda761300b
Orkun Manap <manaporkun@protonmail.com>
2021-08-27 12:04:50 +0300

    Init Commit

---
+++ hello_cargo/target/debug/deps/hello_cargo-845985161df4db98.d
@@ -0,0 +1,5 @@
+/home/plioustm/rust-programming/hello_cargo/target/debug/deps/hello_cargo-845985161df4db98: src/main.rs
+
+/home/plioustm/rust-programming/hello_cargo/target/debug/deps/hello_cargo-845985161df4db98.d: src/main.rs
+
+src/main.rs:
