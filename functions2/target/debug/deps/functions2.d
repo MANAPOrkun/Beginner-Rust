94317433081cbb7709c41928fe63a2587073aff8
Orkun Manap <manaporkun@protonmail.com>
2021-08-31 16:26:19 +0300

    functions2 build

---
+++ functions2/target/debug/deps/functions2.d
@@ -0,0 +1,5 @@
+C:\Users\manaporkun\Documents\Beginner-Rust\functions2\target\debug\deps\functions2.exe: src\main.rs
+
+C:\Users\manaporkun\Documents\Beginner-Rust\functions2\target\debug\deps\functions2.d: src\main.rs
+
+src\main.rs:
---
+++ functions2/target/debug/functions2.d
@@ -0,0 +1,1 @@
+C:\Users\manaporkun\Documents\Beginner-Rust\functions2\target\debug\functions2.exe: C:\Users\manaporkun\Documents\Beginner-Rust\functions2\src\main.rs
