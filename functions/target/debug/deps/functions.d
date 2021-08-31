bd53452bbb604337a0af4d694b8bc23d340b1530
Orkun Manap <manaporkun@protonmail.com>
2021-08-31 16:08:43 +0300

    Functions #1

---
+++ functions/target/debug/deps/functions.d
@@ -0,0 +1,5 @@
+C:\Users\manaporkun\Documents\Beginner-Rust\functions\target\debug\deps\functions.exe: src\main.rs
+
+C:\Users\manaporkun\Documents\Beginner-Rust\functions\target\debug\deps\functions.d: src\main.rs
+
+src\main.rs:
---
+++ functions/target/debug/functions.d
@@ -0,0 +1,1 @@
+C:\Users\manaporkun\Documents\Beginner-Rust\functions\target\debug\functions.exe: C:\Users\manaporkun\Documents\Beginner-Rust\functions\src\main.rs
