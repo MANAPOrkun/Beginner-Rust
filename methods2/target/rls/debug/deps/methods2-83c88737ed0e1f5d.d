d73926de7e89b8b604dc619b80286429d2e80601
Orkun Manap <manaporkun@protonmail.com>
2021-09-06 16:30:08 +0300

    Methods with more parameters

---
+++ methods2/target/rls/debug/deps/methods2-83c88737ed0e1f5d.d
@@ -0,0 +1,5 @@
+c:\Users\manaporkun\Documents\Beginner-Rust\methods2\target\rls\debug\deps\methods2-83c88737ed0e1f5d.rmeta: src\main.rs
+
+c:\Users\manaporkun\Documents\Beginner-Rust\methods2\target\rls\debug\deps\methods2-83c88737ed0e1f5d.d: src\main.rs
+
+src\main.rs:
---
+++ methods2/target/rls/debug/deps/methods2-dd73fbe61ff357c0.d
@@ -0,0 +1,5 @@
+c:\Users\manaporkun\Documents\Beginner-Rust\methods2\target\rls\debug\deps\methods2-dd73fbe61ff357c0.rmeta: src\main.rs
+
+c:\Users\manaporkun\Documents\Beginner-Rust\methods2\target\rls\debug\deps\methods2-dd73fbe61ff357c0.d: src\main.rs
+
+src\main.rs:
