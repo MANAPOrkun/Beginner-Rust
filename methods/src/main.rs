aab88cca933dc220beeca31596842f03d8972616
Orkun Manap <manaporkun@protonmail.com>
2021-09-06 16:22:12 +0300

    Some explanations

--- methods/src/main.rs
+++ methods/src/main.rs
@@ -1,3 +1,12 @@
+/*
+ The main benefit of using methods instead of functions,
+ in addition to using method syntax and not having to repeat 
+ the type of self in every method’s signature, is for organization. 
+ We’ve put all the things we can do with an instance of a type in one 
+ impl block rather than making future users of our code search for capabilities
+ of Rectangle in various places in the library we provide. 
+*/
+
 #[derive(Debug)]
 struct Rectangle {
     width: u32,
@@ -12,6 +21,14 @@ struct Rectangle {
  borrow self immutably as we’ve done here, or borrow self mutably,
  just as they can any other parameter.
 */
+/*
+ We’ve chosen &self here for the same reason we used &Rectangle
+ in the function version: we don’t want to take ownership,
+ and we just want to read the data in the struct, not write to it.
+ If we wanted to change the instance that we’ve called the method on
+ as part of what the method does, we’d use &mut self as the first parameter.
+*/
+
 impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
